e73a750610dad8fa126d0a036cd3d94fd66cfc4c
Paabl0Dev <sapula.pawel@gmai.com>
2025-10-01 18:23:34 +0200

    Tweaks and new classes for styles and the loader. Aesthetic changes and added source codes.

--- tools/loader.js
+++ tools/loader.js
@@ -3,11 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   paragraphs.forEach(div => {
     const file = div.getAttribute("data-file");
+    let separate = div.getAttribute("separate");
 
     fetch(file)
       .then(response => response.text())
       .then(data => {
-        div.innerHTML = `<p>${data}</p>`;
+
+        if(separate) {
+          //const normalizedData = data.replace(/\r\n|\r/g, '\n ');
+          const lines = data.split("\n")
+          lines.forEach((line) => {
+            div.innerHTML += `<p>${line}<br></p>`;   
+          })
+            
+        }
+        else{
+          div.innerHTML = `<p>${data}</p>`;
+        }
+      
       })
       .catch(err => {
         div.innerHTML = `<p style="color:red;">Error loading ${file}</p>`;
